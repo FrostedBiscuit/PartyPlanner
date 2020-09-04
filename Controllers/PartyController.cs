5f6fccde84b8093baa9e53bbf429a77f5b2dfc61
FrostedBiscuit <kaucic.gasper@gmail.com>
2020-09-04 15:20:48 +0200

    Initial commit

---
+++ Controllers/PartyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyPlanner.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PartyController : ControllerBase
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return new { HelloMessage = "Hello world" };
+        }
+    }
+
+    public class TestResponse
+    {
+        public string HelloMessage { get; set; }
+    }
+}
