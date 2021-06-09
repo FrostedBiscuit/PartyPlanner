86ca1584f00eb19f33f6de905998f2f4cdc50d20
Frost <kaucic.gasper@gmail.com>
2021-06-09 15:30:34 +0200

    Consts updated.

--- partyplanner.web/src/core/Consts.js
+++ partyplanner.web/src/core/Consts.js
@@ -1,3 +1,8 @@
 export const ROUTES = {
     GET_PARTY: "api/party/{pid}",
+    CREATE_PARTY: "api/party?name={pname}",
+
+    CREATE_CATEGORY: "api/category/{pid}",
+
+    UPDATE_PARTYINFO: "api/partyInfo/{pid}",
 };
